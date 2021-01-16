4f6168f4b129d5ad918d3849ac50ba5cabb72936
mackxxx <42348939+mackxxx@users.noreply.github.com>
2021-01-17 02:23:46 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'messages/:id', to: 'messages#show'
+  post 'messages', to: 'messages#create'
+  put 'messages/:id', to: 'messages#update'
+  delete 'messages/:id', to: 'messages#destroy'
 end
